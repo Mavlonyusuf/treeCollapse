e11d3c29b95268037b3f4a8c469dc6b7fb8ce790
Mavlon_Yusuf <mavlonjonyusupov1@gmail.com>
2024-04-08 20:22:25 +0500

    tree-collapse finished
    tree-collapse finished

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="wrapper">
+    <TreeCollapse />
+  </div>
+</template>
+<script setup>
+import TreeCollapse from "./props/collapseProp.vue";
+</script>
+<style scoped>
+.wrapper {
+  width: 1000px;
+  margin: 200px auto;
+}
+</style>
